7c8906a62e61a7f665db61c03edd242b4c8dee1b
Chandra Komara <chandrakomara.ck@gmail.com>
2024-06-28 21:54:25 +0700

    update sql

--- sql/tamuku.sql
+++ sql/tamuku.sql
@@ -12,6 +12,11 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+-- Dumping database structure for tamuku
+CREATE DATABASE IF NOT EXISTS `tamuku` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `tamuku`;
+
 -- Dumping structure for table tamuku.ci_sessions
 CREATE TABLE IF NOT EXISTS `ci_sessions` (
   `id` varchar(128) NOT NULL,
@@ -22,7 +27,7 @@ CREATE TABLE IF NOT EXISTS `ci_sessions` (
   KEY `ci_sessions_timestamp` (`timestamp`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table tamuku.ci_sessions: ~2 rows (approximately)
+-- Dumping data for table tamuku.ci_sessions: ~0 rows (approximately)
 DELETE FROM `ci_sessions`;
 /*!40000 ALTER TABLE `ci_sessions` DISABLE KEYS */;
 /*!40000 ALTER TABLE `ci_sessions` ENABLE KEYS */;
@@ -44,7 +49,7 @@ INSERT INTO `configs` (`id`, `key`, `value`, `category`, `note`) VALUES
 	(1, 'APP_VERSION', '1.0', 5, 'string. versi aplikasi'),
 	(2, 'APP_NAME', 'Buku Tamu Digital', 5, 'string. nama aplikasi'),
 	(3, 'APP_SHORT_NAME', 'eTamu', 5, 'string. nama pendek aplikasi. [BACKUP - NANTI DIHAPUS]'),
-	(4, 'SATKER_NAME', 'Pengadilan Agama ABC', 1, 'string. nama satker'),
+	(4, 'SATKER_NAME', 'Kemenag Kabupaten ABC', 1, 'string. nama satker'),
 	(5, 'SATKER_ADDRESS', 'Jalan ABC No. 123', 1, 'string. nama satker'),
 	(6, 'DIALOGWA_API_URL', 'https://dialogwa.id/api', 2, 'string. url api dialogwa.id'),
 	(7, 'DIALOGWA_TOKEN', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZjNiMjIyZWY1MmJjMzc4MDYxM2U1OSIsInVzZXJuYW1lIjoiY2hhbmRyYSIsImlhdCI6MTcxNzc0Nzc4NywiZXhwIjo0ODczNTA3Nzg3fQ.KIqEs7rELJzVj2hk6WJqCiYy0T0Mz7G5vbiy4gFLRQ0', 2, 'string. token dialogwa.id'),
@@ -72,12 +77,31 @@ CREATE TABLE IF NOT EXISTS `guests` (
   PRIMARY KEY (`id`),
   KEY `FK_guests_persons` (`person_to_meet`),
   CONSTRAINT `FK_guests_persons` FOREIGN KEY (`person_to_meet`) REFERENCES `persons` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Dumping data for table tamuku.guests: ~0 rows (approximately)
 DELETE FROM `guests`;
 /*!40000 ALTER TABLE `guests` DISABLE KEYS */;
 /*!40000 ALTER TABLE `guests` ENABLE KEYS */;
+
+-- Dumping structure for table tamuku.persons
+CREATE TABLE IF NOT EXISTS `persons` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `order` tinyint(2) NOT NULL DEFAULT '0',
+  `person` varchar(100) NOT NULL COMMENT 'yang dituju, bisa jabatan atau nama',
+  `gender` enum('L','P') NOT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table tamuku.persons: ~0 rows (approximately)
+DELETE FROM `persons`;
+/*!40000 ALTER TABLE `persons` DISABLE KEYS */;
+INSERT INTO `persons` (`id`, `order`, `person`, `gender`, `phone`) VALUES
+	(1, 1, 'Kepala Kemenag', 'L', '089122345678'),
+	(2, 2, 'Kepala Sub Bagian Tata Usaha', 'P', '0811111111'),
+	(3, 10, 'Fulanah binti Fulan', 'P', '08222222221');
+/*!40000 ALTER TABLE `persons` ENABLE KEYS */;
 
 -- Dumping structure for table tamuku.trans_message_whatsapp
 CREATE TABLE IF NOT EXISTS `trans_message_whatsapp` (
@@ -91,7 +115,7 @@ CREATE TABLE IF NOT EXISTS `trans_message_whatsapp` (
   `success` tinyint(1) NOT NULL DEFAULT '0',
   `note` varchar(100) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table tamuku.trans_message_whatsapp: ~0 rows (approximately)
 DELETE FROM `trans_message_whatsapp`;
@@ -110,7 +134,7 @@ CREATE TABLE IF NOT EXISTS `tref_menu` (
   `order` tinyint(2) DEFAULT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table tamuku.tref_menu: ~3 rows (approximately)
 DELETE FROM `tref_menu`;
@@ -118,7 +142,8 @@ DELETE FROM `tref_menu`;
 INSERT INTO `tref_menu` (`id`, `label`, `parent`, `icon`, `iconClass`, `menuClass`, `url`, `order`, `status`) VALUES
 	(1, 'Form Tamu', NULL, 'pencil-square-o', NULL, NULL, 'etamu', 1, 1),
 	(2, 'Riwayat Tamu', NULL, 'address-book', NULL, NULL, 'etamu/history', 2, 1),
-	(3, 'Riwayat Notifikasi', NULL, 'whatsapp', NULL, NULL, 'whatsapp', 3, 1);
+	(3, 'Riwayat Notifikasi', NULL, 'whatsapp', NULL, NULL, 'whatsapp', 3, 1),
+	(4, 'Daftar Pejabat', NULL, 'users', NULL, NULL, 'etamu/person', 4, 1);
 /*!40000 ALTER TABLE `tref_menu` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
